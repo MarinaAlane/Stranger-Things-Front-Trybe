600b440ff719c7e37e98cf5b5f01b84d514f8159
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-10-20 17:11:23 -0300

    Criada a tag de em desenvolvimento

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 import StrangerThings from './components/StrangerThings';
+import DevelopmentEnv from './components/InDevelopment';
 
 function App() {
   return (
     <div className="App">
+      { process.env.IN_DEVELOPMENT ? <DevelopmentEnv /> : '' }
       <StrangerThings />
     </div>
   );
---
+++ src/components/InDevelopment.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const DevelopmentEnv = () => <h1 className="dev-env">Em desenvolvimento</h1>;
+
+export default DevelopmentEnv;
